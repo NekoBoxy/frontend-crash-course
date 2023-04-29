a645e556fe8e7180a971cc26b49986dcb2eec64b
CatBoxy <catboxy@gmail.com>
2023-04-29 21:41:55 +0800

    categories 大致OK，剩背景顏色需調整

--- assignment0/src/views/HomeView.vue
+++ assignment0/src/views/HomeView.vue
@@ -212,7 +212,7 @@
       <!-- categories -->
       <h3 class="text-center">Categories</h3>
       <p class="d-block text-center mb-5">Find what you are looking for</p>
-      <div class="col-12 mt-4" style="height: 841px; background-color: #C1DCDC; position: relative;">
+      <div class="col-12 mt-4 text-center" style="height: 841px; background-color: #C1DCDC; position: relative;">
         <div class="cate-cards">
           <div class="row">
             <div class="col-4 card noline-card cate-img" style="top: -48px;">
@@ -235,10 +235,15 @@
             </div>
           </div>
         </div>
-        <span class="d-block text-center">Horem ipsum dolor sit amet,</span>
-        <span class="d-block text-center">consectetur adipiscing elit.</span>
+        <div>
+          <div class="mt-4 mb-3">
+            <span class="d-block text-center">Horem ipsum dolor sit amet,</span>
+            <span class="d-block text-center">consectetur adipiscing elit.</span>
+          </div>
+        </div>
         <div class="d-flex justify-content-center">
-          <button type="button" class="btn btn-outline-secondary">Explore
+          <button type="button" class="btn btn-outline-secondary"
+            style="border-radius: 8px; background-color: #ffffff; width: 147px; height: 51px; font-weight: 500; font-size: 18px; line-height: 27px; color: #1E1E1E;">Explore
             <svg width="22" height="13" viewBox="0 0 22 13" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path fill-rule="evenodd" clip-rule="evenodd"
                 d="M0.5 6.50006C0.5 6.3343 0.579018 6.17533 0.71967 6.05812C0.860322 5.94091 1.05109 5.87506 1.25 5.87506H18.9395L14.219 1.94256C14.0782 1.8252 13.9991 1.66603 13.9991 1.50006C13.9991 1.33409 14.0782 1.17492 14.219 1.05756C14.3598 0.940199 14.5508 0.874268 14.75 0.874268C14.9492 0.874268 15.1402 0.940199 15.281 1.05756L21.281 6.05756C21.3508 6.11561 21.4063 6.18458 21.4441 6.26052C21.4819 6.33645 21.5013 6.41785 21.5013 6.50006C21.5013 6.58227 21.4819 6.66367 21.4441 6.7396C21.4063 6.81553 21.3508 6.8845 21.281 6.94256L15.281 11.9426C15.1402 12.0599 14.9492 12.1258 14.75 12.1258C14.5508 12.1258 14.3598 12.0599 14.219 11.9426C14.0782 11.8252 13.9991 11.666 13.9991 11.5001C13.9991 11.3341 14.0782 11.1749 14.219 11.0576L18.9395 7.12506H1.25C1.05109 7.12506 0.860322 7.05921 0.71967 6.942C0.579018 6.82479 0.5 6.66582 0.5 6.50006Z"
@@ -379,6 +384,10 @@ p {
   color: #1E1E1E80;
 }
 
+button {
+  border: none;
+}
+
 /* landing page */
 .div-right-line::after {
   content: "";
@@ -430,8 +439,12 @@ p {
   position: relative;
 }
 
-.cate-cards .cate-img img {
+.cate-img img {
   height: 512px;
+}
+
+.cate-img div {
+  padding-top: 12px;
 }
 
 .cate-cards h5 {
