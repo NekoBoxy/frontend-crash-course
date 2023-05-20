9d5cce21ae64e11627509ac7125ad5f2db874851
CatBoxy <catboxy@gmail.com>
2023-05-21 00:39:52 +0800

    clear all 的 fn DONE

--- assignment3/index02.js
+++ assignment3/index02.js
@@ -12,7 +12,7 @@ const submitBtn = document.getElementsByClassName(".submitBtn");
 // console.log("submitBtn", submitBtn);
 
 // step 1.2 建立資料格式(陣列)與數量
-const todoDatas = [
+let todoDatas = [
   {
     id: 0,
     content: "吃飯",
@@ -65,35 +65,49 @@ function newDom({ id, content }) {
 function deleteTodo(element) {
   // want: 抓到指定 id 後刪掉整個 DOM
   // 按下 btn 後往上找父層的 data-id <div data-id="">...</div>
-  console.log("flag2");
-  const idStr = element.target.parentNode.getAttribute("data-id");
-  const id = parseInt(idStr);
-  console.log(id);
-  console.log(idStr);
+  const elDiv = document.querySelector("data-id");
+  console.log("elDiv", elDiv);
+  console.log("element", element);
+
 };
 
-// function getDom(element) {
-//   console.log("flag2");
-//   const idStr = element.target.parentNode.getAttribute("data-id");
-//   const id = parseInt(idStr);
-//   console.log(id);
-//   console.log(idStr);
+// 將 .list-Area 內的所有 div 與陣列資料清空
+function deleteAll() {
+  dataLen = todoDatas.length;
+  const parentNode = document.querySelector(".list-Area");
+  // console.log(parentNode); // <div class="list-Area">...</div>
+  const elDivNodeList = document.querySelectorAll(".list-Area div");
+  // console.log("elDivNodeList", elDivNodeList); // nodelist(4) [div, div, div, div]
+  console.log(dataLen); // 4
+  for (let i = 0; i <= dataLen - 1; i++) {
+    let delNode = elDivNodeList[i]
+    parentNode.removeChild(delNode);
+  }
+  // 清空陣列資料
+  todoDatas = [];
+  console.log(todoDatas.length); // 0
+  changeTotalDom(0);
+};
 
-// };
 
 // 修改現有 todo 數字
 function initTotalDom() {
   let elDivNodeList = document.getElementsByClassName("total"); // 回傳結果是 陣列
   let elDiv = elDivNodeList[0]; // 只有一筆，所以 i=0
   elDiv.textContent = dataLen; // 把數字改成陣列長度(資料筆數)
-  console.log("initTotal", elDiv.textContent);
+  // console.log("initTotal", elDiv.textContent);
 };
 
+// data 更新後修改顯示的 todo 數字
 function changeTotalDom(id) {
   let elDivNodeList = document.getElementsByClassName("total"); // 回傳結果是 陣列
   let elDiv = elDivNodeList[0]; // 只有一筆，所以 i=0
-  elDiv.textContent = id + 1; // 把數字改成陣列長度(資料筆數)
-  console.log("changeTotal", elDiv.textContent);
+  if (todoDatas.length >= 1) {
+    elDiv.textContent = id + 1; // todoDatas 有資料時，把數字改成陣列長度(資料筆數)
+  } else {
+    elDiv.textContent = id; // id = 0
+  }
+  // console.log("changeTotal", elDiv.textContent);
 };
 
 // todo list 的初次渲染
@@ -104,7 +118,5 @@ function init() {
   }
   initTotalDom();
 };
-
-
 
 init();
