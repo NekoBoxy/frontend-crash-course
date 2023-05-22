b0377ea7a75cac7b0ff1ca6a01f4991f699184e2
CatBoxy <catboxy@gmail.com>
2023-05-22 11:53:44 +0800

    deploy

--- assignment1/assignment1/practice5.js
+++
@@ -1,5 +0,0 @@
-// practice of function
-// Declare a arrow function called isEven, should pass a number variable and should return the result if is even return true
-// Declare a arrow function called isLeapYear, should pass a number variable and should return the result if is leap year return true
-// Declare a arrow function called isPrime, should pass a number variable and should return the result if is prime number return true
-// Declare a arrow function called findMax, should pass 3 number variables and should return the max number of them.
