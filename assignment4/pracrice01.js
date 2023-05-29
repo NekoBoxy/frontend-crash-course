094b115023e6d36e7dadfabf0aa3f4bb35c91605
CatBoxy <catboxy@gmail.com>
2023-05-29 23:52:20 +0800

    4-practice2 DONE

--- assignment4/pracrice01.js
+++ assignment4/pracrice01.js
@@ -2,11 +2,10 @@
 // 使用 Promise，在延遲 1 秒後，在控制台打印出 "Hello, world!"。
 const myPromise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    const data = "順利完成 1";
-    resolve(data);
-  }, 2000);
-})
-
-myPromise.then((data) => {
-  console.log("順利完成 2", data);
+    const msg = "Hello, world!";
+    resolve(msg);
+  }, 1000);
+});
+myPromise.then((msg) => {
+  console.log(msg);
 });
--- practice/class4.js
+++ practice/class4.js
@@ -1,34 +1,58 @@
-// let count = 0;
-// const interval = setInterval(function () {
-//   console.log("count", count);
-//   count++;
-//   if (count === 5) {
-//     clearInterval(interval); // 會中斷並取消掉 setinterval
-//   }
-// }, 2000)
-
-const myPromise = new Promise((result, reject) => {
-  setTimeout(() => {
-    const data = "成功";
-    console.log(data);
-    const error = new Error("失敗");
-    reject(error);
-  }, 1000)
-});
-
-// myPromise.then((el) => {
-//   console.log(el);
-// });
-
-myPromise.catch((el) => {
-  console.log("error");
-});
-
-// 
-console.log("step 1");
-const promise = new Promise((resolve, reject) => {
-  setTimeout(function () {
-    console.log();
-    resolve();
+// 範例 1 by 卡斯伯
+let status = true;
+const boxyPromise = function (status) {
+  return new Promise(function (resolve, reject) {
+    // // 改寫為箭頭函式
+    // const boxyPromise = (status) => {
+    //   return new Promise((resolve, reject) => {
+    setTimeout(function () {
+      if (status) {
+        resolve("boxyPromise 成功");
+      } else {
+        reject("boxyPromise 失敗");
+      }
+    }, 0);
   })
+};
+
+// 接收成功的結果
+boxyPromise(true)
+  // .then(function ("成功訊息") {
+  .then(function (response) {
+    console.log(response);
+  });
+
+// // 接收失敗的結果
+// boxyPromise(false)
+//   .then(res => {
+//     console.log(res);
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+
+
+// 範例 2 丹尼
+// 成功的範例
+const myPromise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const data = "你成功惹";
+    resolve(data);
+  }, 1000);
+});
+
+myPromise.then((data) => {
+  console.log(data);
+});
+
+// 失敗的接收
+const myPromise2 = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    const data2 = "你失敗惹";
+    reject(data2);
+  }, 2500);
+});
+
+myPromise2.catch((data2) => {
+  console.log(data2);
 });
