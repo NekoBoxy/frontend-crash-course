f96e5a80d1feeeb3a7bb8c41c973e8d9e986ddb7
CatBoxy <catboxy@gmail.com>
2023-05-30 10:57:40 +0800

    deploy

--- assignment4/pracrice01.js
+++ assignment4/pracrice01.js
@@ -1,11 +1,36 @@
 // 顯示一個訊息：
 // 使用 Promise，在延遲 1 秒後，在控制台打印出 "Hello, world!"。
-const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    const msg = "Hello, world!";
-    resolve(msg);
-  }, 1000);
-});
-myPromise.then((msg) => {
-  console.log(msg);
-});
+// const myPromise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     const msg = "Hello, world!";
+//     resolve(msg);
+//   }, 1000);
+// });
+// myPromise.then((msg) => {
+//   console.log(msg);
+// });
+
+// 串接 API
+const predictInput = document.getElementById("predictInput");
+const predictBtn = document.getElementById("predictBtn");
+const predictOutput = document.getElementById("predictOutput");
+
+function getActivity() {
+  const xhr = new XMLHttpRequest();
+  const method = "GET";
+  const url = "https://www.boredapi.com/api/activity";
+  xhr.open(method, url);
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      // step 取得 api 回傳的資料並轉成純文字
+      // console.log("xhr.responseText:", xhr.responseText);
+      const json = JSON.parse(xhr.responseText);
+      const text = json.activity;
+      // 更改指定 DOM 的文字內容
+      target.innerHTML = text;
+    } else {
+      console.error("status:", xhr.status);
+    }
+  };
+  xhr.send(); // 要有這個才收的到戳 API 的回傳結果
+};
--- assignment4/pracrice02.js
+++ assignment4/pracrice02.js
@@ -2,31 +2,31 @@
 // 使用 async / await，創建一個函數，該函數將延遲 3 秒後返回一個 Promise。然後使用 await 來等待 Promise 完成，並在控制台打印出 "Delay complete!"。
 
 // 寫法 1：以立即函式執行
-const myPromise2 = (status) => {
-  return new Promise((resolve, reject) => {
-    if (status === true) {
-      setTimeout(() => {
-        const msg = "Delay complete!";
-        resolve(msg);
-      }, 3000)
-    } else {
-      reject("失敗");
-    }
-  })
-};
+// const myPromise1 = (status) => {
+//   return new Promise((resolve, reject) => {
+//     if (status === true) {
+//       setTimeout(() => {
+//         const msg = "Delay complete!";
+//         resolve(msg);
+//       }, 3000)
+//     } else {
+//       reject("失敗");
+//     }
+//   })
+// };
 
-(async () => {
-  await myPromise2(true)
-    .then((res) => {
-      console.log("res", res);
-    })
-    .catch((err) => {
-      console.log("err", err);
-    });
-})();
+// (async () => {
+//   await myPromise1(true)
+//     .then((res) => {
+//       console.log("res", res);
+//     })
+//     .catch((err) => {
+//       console.log("err", err);
+//     });
+// })();
 
 // 寫法 2：以 try...catch 包覆，再用 fn 執行
-// const myPromise2 = (status) => {
+// const myPromise1 = (status) => {
 //   return new Promise((resolve, reject) => {
 //     if (status === true) {
 //       setTimeout(() => {
@@ -41,7 +41,7 @@ const myPromise2 = (status) => {
 
 // async function getPromise(status) {
 //   try {
-//     const value = await myPromise2(status);
+//     const value = await myPromise1(status);
 //     console.log("value", value);
 //   } catch (err) {
 //     console.log("err", err);
