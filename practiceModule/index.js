789e65ece7db169194b2b3c42266cb9bde1b2a84
CatBoxy <catboxy@gmail.com>
2023-06-01 11:20:43 +0800

    bootstrap init

---
+++ practiceModule/index.js
@@ -0,0 +1,4 @@
+import * as math from "./math.js";
+
+console.log(math.pi);
+console.log(math.add(3, 4));
---
+++ practiceModule/math.js
@@ -0,0 +1,5 @@
+export const pi = 3.14;
+
+export function add(x, y) {
+  return x + y;
+};
