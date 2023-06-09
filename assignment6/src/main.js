31ad4d34d696c71504c210d45fd07554c4a54d5c
CatBoxy <catboxy@gmail.com>
2023-06-09 20:09:59 +0800

    init

---
+++ assignment6/src/main.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+import * as bootstrap from 'bootstrap'
+// 載入自定義的 css
+import './assets/all.css'
+
+import { createApp } from 'vue'
+import { createPinia } from 'pinia'
+
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
---
+++ assignment6/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <CNavbar />
+  </header>
+  <main></main>
+  <footer></footer>
+</template>
+
+<script setup>
+import CNavbar from '../components/CNavbar.vue';
+</script>
+
