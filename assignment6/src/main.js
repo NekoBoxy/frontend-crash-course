55ca0a46702cc79865223e80b9a44afb31401deb
CatBoxy <catboxy@gmail.com>
2023-07-07 16:25:41 +0800

    調整空白行

--- assignment6/src/main.js
+++ assignment6/src/main.js
@@ -1,18 +1,17 @@
 // eslint-disable-next-line no-unused-vars
-import * as bootstrap from 'bootstrap'
+import * as bootstrap from 'bootstrap';
 // 載入自定義的 css
-import './assets/all.css'
+import './assets/all.css';
 
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
 
+import App from './App.vue';
+import router from './router';
 
-import App from './App.vue'
-import router from './router'
+const app = createApp(App);
 
-const app = createApp(App)
+app.use(createPinia());
+app.use(router);
 
-app.use(createPinia())
-app.use(router)
-
-app.mount('#app')
+app.mount('#app');
