0a0377a0bbdc7de1ea00162ccb63e9a0bb062a67
CatBoxy <catboxy@gmail.com>
2023-07-27 21:43:08 +0800

    隱去pinia

--- assignment6/src/main.js
+++ assignment6/src/main.js
@@ -4,14 +4,14 @@ import * as bootstrap from 'bootstrap';
 import './assets/all.css';
 
 import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-
+// import { createPinia } from 'pinia';
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
 
-app.use(createPinia());
+// app.use(createPinia());
 app.use(router);
+
 
 app.mount('#app');
