5183aa09d6f0c63e05d8d4f6ad6a9f7564c4f644
CatBoxy <catboxy@gmail.com>
2023-07-28 20:37:11 +0800

    navbar增加VueMultiselect, 未完成

--- assignment6/src/components/CNavbar.vue
+++ assignment6/src/components/CNavbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <nav class="navbar navbar-expand-lg">
     <div class="container">
       <RouterLink to="/" class="navbar-brand nav-logo">
         <svg width="26" height="50" viewBox="0 0 26 50" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -19,11 +19,6 @@
       <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav mb-2 mb-lg-0 ms-auto">
           <li class="nav-item">
-            <div class="nav-link" aria-current="true">
-              <RouterLink to="/">首頁</RouterLink>
-            </div>
-          </li>
-          <li class="nav-item">
             <div class="nav-link">
               <RouterLink to="/points/Taipei">旅遊景點</RouterLink>
             </div>
@@ -33,16 +28,21 @@
               <RouterLink to="/foods/NewTaipei">美食餐廳</RouterLink>
             </div>
           </li>
-          <form class="d-flex">
-            <input class="form-control me-2" type="search" placeholder="想去哪裡玩？" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">
+          <div class="d-flex">
+            <VueMultiselect v-model="selectStr" :options="compareData" @search-change="callApi" placeholder="想去哪玩呢？"
+              label="value" :loading="isLoading" :show-labels="false">
+              <template #noResult>
+                查無結果，請更換關鍵字。
+              </template>
+            </VueMultiselect>
+            <button class="btn btn-outline-success" type="submit" @click="handleSearchClick(el)">
               <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" clip-rule="evenodd"
                   d="M10.6798 4C7.01399 4 4.0423 6.91015 4.0423 10.5C4.0423 14.0899 7.01399 17 10.6798 17C14.3455 17 17.3172 14.0899 17.3172 10.5C17.3172 6.91015 14.3455 4 10.6798 4ZM2 10.5C2 5.80558 5.88606 2 10.6798 2C15.4735 2 19.3595 5.80558 19.3595 10.5C19.3595 12.5926 18.5873 14.5087 17.3067 15.9897L21.7009 20.2929C22.0997 20.6834 22.0997 21.3166 21.7009 21.7071C21.3021 22.0976 20.6556 22.0976 20.2568 21.7071L15.8127 17.3551C14.3751 18.3892 12.6003 19 10.6798 19C5.88606 19 2 15.1944 2 10.5Z"
                   fill="#392A93" />
               </svg>
             </button>
-          </form>
+          </div>
         </ul>
       </div>
     </div>
@@ -50,13 +50,88 @@
 </template>
 
 <script setup>
-import { RouterLink } from 'vue-router';
+import "~multiselect/dist/vue-multiselect.css";
+import { ref, watch } from 'vue';
+import { RouterLink, useRouter, } from 'vue-router';
+import axios from 'axios';
+import VueMultiselect from 'vue-multiselect';
 
+const router = useRouter();
+
+// autocomoplete
+const selectStr = ref("");
+const compareData = ref([]);
+const cityList = ref([
+  { title: "基隆市", name: "Keelung" },
+  { title: "臺北市", name: "Taipei" },
+  { title: "新北市", name: "NewTaipei" },
+  { title: "桃園市", name: "Taoyuan" },
+  { title: "苗栗縣", name: "MiaoliCounty" },
+  { title: "新竹縣", name: "HsinchuCounty" },
+  { title: "新竹市", name: "Hsinchu" },
+  { title: "臺中市", name: "Taichung" },
+  { title: "彰化縣", name: "ChanghuaCounty" },
+  { title: "南投縣", name: "NantouCounty" },
+  { title: "雲林縣", name: "YunlinCounty" },
+  { title: "嘉義縣", name: "ChiayiCounty" },
+  { title: "嘉義市", name: "Chiayi" },
+  { title: "臺南市", name: "Tainan" },
+  { title: "高雄市", name: "Kaohsiung" },
+  { title: "屏東縣", name: "PingtungCounty" },
+  { title: "宜蘭縣", name: "YilanCounty" },
+  { title: "花蓮縣", name: "HualienCounty" },
+  { title: "臺東縣", name: "TaitungCounty" },
+  { title: "金門縣", name: "KinmenCounty" },
+  { title: "澎湖縣", name: "PenghuCounty" },
+  { title: "連江縣", name: "LienchiangCounty" },
+]);
+const isLoading = ref(false);
+
+watch(selectStr, (newValue) => {
+  console.log("Selected:", newValue);
+});
+
+function callApi(query) {
+  isLoading.value = true;
+  console.log(query);
+  // let result = [];
+  // result = cityList.value.map();
+
+  // // Call API
+  // // query
+  // result = [...result, apiRes];
+
+  // compareData.value = result;
+}
+
+// call api
+async function getTdxData(str) {
+  try {
+    const response = await axios({
+      method: "get",
+      url: `${import.meta.env.VITE_BASE_URL}/v2/Tourism/ScenicSpot`,
+      params: {
+        "$top": "5",
+        "$filter": `City eq '${str}' or Keyword eq '${str}' or ScenicSpotName eq '${str}'`,
+        "$format": "JSON"
+      }
+    });
+    console.log("response.data", response.data);
+
+  } catch (error) {
+    alert(error);
+  }
+}
+
+// 路由跳轉
+async function changeRoute(data) {
+  await router.push({ path: `/points/${data.cityEn}` });
+  await router.go();
+}
 </script>
 
 <style scoped lang="scss">
 nav {
-  color: #392A93;
   border-bottom: 1px solid #0000001A;
   margin-bottom: 20px;
 }
@@ -92,6 +167,11 @@ nav {
 
 .nav-link a {
   color: #907df7;
+
+  &:hover {
+    box-shadow: 0px 3px 0px 0px #907df7;
+    font-weight: 700;
+  }
 }
 
 .router-link-exact-active {
